aab7e270ca15859622df164ee936ec8e9b0b4cc8
Jack Palmer <palmerjw@iu.edu>
2021-07-25 00:19:13 -0400

    made some progress on how the game should look

---
+++ solitare-gambling/src/Components/card-helper-functions.component.js
@@ -0,0 +1,23 @@
+export let fontString = "500 normal 16px Dejavu Serif"
+export function getCardHeight(containerHeight){
+    const tempHeight = Math.ceil(containerHeight / 100)/1.23 ;
+    if(tempHeight < 5)
+    {
+        return 5
+    }
+    return tempHeight
+}
+export function getAdjustment(cardWidth){
+    return Math.ceil(cardWidth/10) + 1;
+    
+}
+
+export function getCardWidth(containerWidth){
+    const tempWidth = Math.ceil(containerWidth / 10)/2;
+    if(tempWidth < 45){
+        return 45
+    }
+
+    return tempWidth
+}
+
